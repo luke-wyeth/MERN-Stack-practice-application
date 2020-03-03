c3c79522adb246e8cbf80647e1b3a71edc49d913
Luke <luke.wyeth@mail.com>
2020-03-03 18:46:09 +1300

    Remove DB in app

--- server/index.js
+++ server/index.js
@@ -7,18 +7,18 @@ const db = require('./db')
 const movieRouter = require('./routes/movie-router')
 
 const app = express()
-const apiPort = 3000
+// const apiPort = 3000
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(cors())
+// app.use(bodyParser.json())
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.use('/api', movieRouter)
+// app.use('/api', movieRouter)
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+// app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
