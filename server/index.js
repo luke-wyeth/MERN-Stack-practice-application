278027907f07168d4f353147cab8d911d43f76b0
Luke <luke.wyeth@mail.com>
2020-03-04 19:43:08 +1300

    missed a quotation

--- server/index.js
+++ server/index.js
@@ -9,16 +9,16 @@ const movieRouter = require('./routes/movie-router')
 const app = express()
 const apiPort = 8000
 
-// app.use(bodyParser.urlencoded({ extended: true }))
-// app.use(cors())
-// app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use(bodyParser.json())
 
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-// app.use('/api', movieRouter)
+app.use('/api', movieRouter)
 
-// app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
