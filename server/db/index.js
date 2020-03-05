0b087b80aaa91460430e4e007d81ac90f1ba786b
Luke <luke.wyeth@mail.com>
2020-03-05 17:17:42 +1300

    try to use new URL parser

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const mongoDB = require('mongodb')
 
 mongoose
-    .connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI || 'mongodb://test_connection:test_connection1@ds243084.mlab.com:43084/heroku_02q9nlhn' )
+    .connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI || 'mongodb://test_connection:test_connection1@ds243084.mlab.com:43084/heroku_02q9nlhn', {useNewUrlParser: true})
     .catch(e => {
         console.log(process.env.MONGODB_URI)
         console.error('Connection error', e.message)
@@ -11,3 +11,4 @@ mongoose
 const db = mongoose.connection
 
 module.exports = db
+
