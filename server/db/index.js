5b674265a45540431a96498bf3c8382754b4ba8a
Luke <luke.wyeth@mail.com>
2020-03-09 18:05:49 +1300

    change connection url

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const mongoDB = require('mongodb')
 
 mongoose
-    .connect('mongodb://test_connection:test_connection1@ds243084.mlab.com:43084/heroku_02q9nlhn', {useNewUrlParser: true})
+    .connect(process.env.MONGODB_URI || 'mongodb://test_connection:test_connection1@ds243084.mlab.com:43084/heroku_02q9nlhn')
     .catch(e => {
         console.log(process.env.MONGODB_URI)
         console.error('Connection error', e.message)
