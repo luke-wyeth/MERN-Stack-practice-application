efb8b0480ed5fd5d5abc33d06dfafbacc394f997
calcalbrad <44259380+calcalbrad@users.noreply.github.com>
2020-03-17 13:48:49 +1300

    Updated Test directory again
    Made a mistake with the last one uploaded whoops

--- test/test.js
+++ test/test.js
@@ -1,7 +1,8 @@
 describe("inc", function(){ 
   it("increments an integer correctly", function() {
     var test = 1;
+    test++
     var assert = require('assert');
-    assert.equal(test++, 2);
+    assert.equal(test, 2);
   });
 });
