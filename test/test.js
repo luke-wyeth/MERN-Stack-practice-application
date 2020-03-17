a55521be41f467a4462743225794676ea2f33105
calcalbrad <44259380+calcalbrad@users.noreply.github.com>
2020-03-17 13:46:59 +1300

    Updated Test directory for Mocha

--- test/test.js
+++ test/test.js
@@ -1,8 +1,7 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
+describe("inc", function(){ 
+  it("increments an integer correctly", function() {
+    var test = 1;
+    var assert = require('assert');
+    assert.equal(test++, 2);
   });
 });
